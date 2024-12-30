fb1f2d187a541a843e139d7ebab962f0793926ec
GraceLove <govlmo91@gmail.com>
2024-12-30 14:24:15 +0900

    Start zettai

--- objects-to-functions/zettai/src/main/kotlin/ui/HtmlPageDsl.kt
+++ objects-to-functions/zettai/src/main/kotlin/ui/HtmlPageDsl.kt
@@ -2,6 +2,7 @@ package ui
 
 import org.http4k.core.*
 import org.http4k.routing.bind
+import org.http4k.routing.path
 import org.http4k.routing.routes
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
@@ -16,12 +17,29 @@ val htmlPage = """
 
 val app: HttpHandler = routes(
     "/greetings" bind Method.GET to ::greetings,
-    "/data" bind Method.POST to ::receiveData
+    "/data" bind Method.POST to ::receiveData,
+    "/todo/{user}/{list}" bind Method.GET to ::showList,
 )
 
 fun greetings(req: Request): Response = Response(Status.OK).body(htmlPage)
 
 fun receiveData(req: Request): Response = Response(Status.CREATED).body("Received ${req.bodyString()}")
+
+fun showList(req: Request): Response {
+    val user: String? = req.path("user")
+    val list: String? = req.path("list")
+    val htmlPage = """
+        <html>
+            <body>
+                <h1 style="text-align:center; font-size:3em;" >Hello Functional World!</h1>
+                <h2>User: $user</h2>
+                <h2>List: $list</h2>
+            </body>
+        </html>
+    """.trimIndent()
+
+    return Response(Status.OK).body(htmlPage)
+}
 
 fun main() {
     app.asServer(Jetty(8080)).start()
