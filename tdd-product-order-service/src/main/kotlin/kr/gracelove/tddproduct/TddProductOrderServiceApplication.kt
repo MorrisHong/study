3d9445df96202280d12d811a470817f94f457638
GraceLove <govlmo91@gmail.com>
2025-01-03 08:29:28 +0900

    tdd product order service init

---
+++ tdd-product-order-service/src/main/kotlin/kr/gracelove/tddproduct/TddProductOrderServiceApplication.kt
@@ -0,0 +1,11 @@
+package kr.gracelove.tddproduct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TddProductOrderServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TddProductOrderServiceApplication>(*args)
+}
---
+++ tdd-product-order-service/src/test/kotlin/kr/gracelove/tddproduct/TddProductOrderServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.gracelove.tddproduct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TddProductOrderServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
